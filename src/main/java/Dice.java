9501881a6b4e9d3b30606e21754c05caed198740
Meredith <meredith@Zipcoders-MacBook-Pro-2.local>
2022-03-11 11:45:39 -0500

    created Dice constructors and tossAndSum method

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,24 @@
 public class Dice {
+    public int numberOfSides;
+    public int numberOfDice;
 
+    // may need to add additional methods
 
+    public Dice(int numberOfDice) {
+        this.numberOfDice = numberOfDice;
+        numberOfSides = 6;
+    }
+
+    public Dice(int numberOfDice, int numberOfSides) {
+        this.numberOfDice = numberOfDice;
+        this.numberOfSides = numberOfSides;
+    }
+
+    public Integer tossAndSum() {
+        Integer sum = 0;
+        for(int i = 1; i <= numberOfDice; i++) {
+            sum += ((int) ((Math.random() * (numberOfSides - 1)) + 1));
+        }
+        return sum;
+    }
 }
