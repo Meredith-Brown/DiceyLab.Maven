d28be2d1542295623d245a51fabe4bc7be292ae6
Meredith <meredith@Zipcoders-MacBook-Pro-2.local>
2022-03-13 00:26:28 -0500

    created Simulation constructor

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,4 +1,11 @@
 public class Simulation {
+    public int numberOfDiesToThrow;
+    public int numberOfTossesToRun;
+
+    public Simulation(int numberOfDiesToThrow, int numberOfTossesToRun) {
+        this.numberOfDiesToThrow = numberOfDiesToThrow;
+        this.numberOfTossesToRun = numberOfTossesToRun;
+    }
 
 
 
